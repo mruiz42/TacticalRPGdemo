906e322e241e22d877cdb8533acee659314ddd66
Michael Ruiz <michael@mruiz.dev>
2020-01-21 14:48:22 -0800

    added directories

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
 
     while (window.isOpen())
     {
