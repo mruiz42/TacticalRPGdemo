456a8b6c695b66554d8e9d18c80874762dc97742
Michael Ruiz <michael@mruiz.dev>
2020-01-27 11:01:52 -0800

    moved vertexmap definitions to .cpp file added window width and height definitions

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,18 @@
 #include <SFML/Graphics/CircleShape.hpp>
 #include "../include/tact/VertexMap.h"
 #include <string>
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
 const std::string root_path = "./";    // Linux
 // Can someone make a standard windows path that will work?
 // const std::string root_path = "C:/";    // Windows
 int main()
 {
     sf::Vertex vertex;
-    sf::RenderWindow window(sf::VideoMode(256, 128), "Tilemap");
-    window.setSize(sf::Vector2u(1024, 512));
+    sf::RenderWindow window(sf::VideoMode(640, 360), "Tilemap");
+    window.setSize(sf::Vector2u(WINDOW_WIDTH, WINDOW_HEIGHT));
     // define the level with an array of tile indices
     const int level[] = {
                       4,  0,  0,  0,  0,  0, 34,  7,  7,  7,  7,  7,  7,  7,  7, 53,
@@ -20,12 +24,12 @@ int main()
                       0, 49, 35,  0,  9, 54, 41,  0,  0,  0,  4,  4,  4,  2,  0,  0,
                       0,  0,  8,  0, 55,  0,  2,  2,  0,  0,  4,  4,  4,  4,  2,  0,
                       2,  0,  8,  0, 55,  0,  2,  2,  2,  0,  4,  4,  4,  4,  4,  4,
-                      0,  0, 51,  0, 55,  2,  2,  2,  0,  0,  0,  0,  4,  4,  4,  4,
+                      0,  0, 51,  0, 55,  2,  2,  2,  0,  0,  0,  0,  4,  4,  4,  4
             };
 
     // create the tilemap from the level definition
     VertexMap map;
-    if (!map.loadMap(root_path + "share/textures/map_tiles.png", sf::Vector2u(16, 16), level, 16, 8))
+    if (!map.loadMap(root_path + "share/textures/map_tiles3.png", sf::Vector2u(16, 16), level, 16, 8))
         return -1;
 
     while (window.isOpen())
