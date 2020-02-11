d65059fc3da7902084b169ae4c5cd6368d32ff86
Michael Ruiz <michael@mruiz.dev>
2020-02-11 14:01:04 -0800

    changed tile array to std::vertex

--- src/main.cpp
+++ src/main.cpp
@@ -27,18 +27,14 @@ int main()
     const unsigned int num_tiles_x = (WINDOW_WIDTH - (TEXTURE_SIZE * 8)) / TEXTURE_SIZE;
     const unsigned int num_tiles_y = WINDOW_HEIGHT / TEXTURE_SIZE;
     const unsigned int num_tiles_total = num_tiles_x * num_tiles_y;
-    sf::Vertex vertex;
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "[Untitled game]");
     window.setSize(sf::Vector2u(WINDOW_WIDTH, WINDOW_HEIGHT));
     // define the level with an array of tile indices
-    int level[num_tiles_x][num_tiles_y] = {};
-    for (int i = 0; i < num_tiles_x; i++)
-        for (int j = 0; j < num_tiles_y; j++)
-        level[i][j] = 1;
+
     // create the tilemap from the level definition
     VertexMap map;
     if (!map.loadMap(root_path + "share/textures/map_tiles32.png", root_path + "share/sprites/cursor.png",
-            sf::Vector2u(TEXTURE_SIZE, TEXTURE_SIZE), level, num_tiles_x, num_tiles_y))
+            sf::Vector2u(TEXTURE_SIZE, TEXTURE_SIZE), num_tiles_x, num_tiles_y))
         return -1;
 
     // Cursor
